f082d34f3e36d149684754f3b186f6c6961b1c06
KseniiaLk <89646252+KseniiaLk@users.noreply.github.com>
2022-06-14 09:10:41 +0200

    New details

---
+++ react-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calendar from './components/Calendar';
+
+
+class App extends React.Component {
+  render(){return (
+    <div >
+   <Calendar/>
+    </div>
+  );}
+ 
+}
+
+export default App;
